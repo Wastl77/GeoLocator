3070552a8686da1ceeaa4ce63d29758ee7f81fc8
Sebastian Beier <sebastianbeier@Sebastians-MacBook-Pro.local>
2018-11-12 21:39:16 +0100

    Convert tabs back to spaces :-)

--- src/js/geocode.js
+++ src/js/geocode.js
@@ -1,11 +1,11 @@
 export function geocodeLocation(lat, lng) {
-	const url="https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyDZhHS4OYBe0AGr1cByePHG-pz8ORJ1KtU"
-	fetch(url)
-	.then(res => {return res.json()})
-	.then(data => {
-		let currentLocation = (data.results[0].address_components[2].long_name);
-		document.getElementById("currentLocation").innerHTML = currentLocation;
-		new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
-	})
-	.catch(error => console.log(error))
+  const url="https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyDZhHS4OYBe0AGr1cByePHG-pz8ORJ1KtU"
+  fetch(url)
+  .then(res => {return res.json()})
+  .then(data => {
+    let currentLocation = (data.results[0].address_components[2].long_name);
+    document.getElementById("currentLocation").innerHTML = currentLocation;
+    new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+  })
+  .catch(error => console.log(error))
 }
