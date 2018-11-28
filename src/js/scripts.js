1a3b2b2ce8e3824fdc44a901b47867dae70090a0
Sebastian Beier <sebastianbeier@Sebastians-MacBook-Pro.local>
2018-11-28 16:32:04 +0100

    Add EventListener in scripts.js, remove "onclick" in index.html

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -20,17 +20,6 @@ import {geocodeLocation} from './geocode.js'
     })
     .catch(error => console.log(error))
   }
-  // function geocodeLocation(lat, lng) {
-  //   const url="https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyDZhHS4OYBe0AGr1cByePHG-pz8ORJ1KtU"
-  //   fetch(url)
-  //   .then(res => {return res.json()})
-  //   .then(data => {
-  //     let currentLocation = (data.results[0].address_components[2].long_name);
-  //     document.getElementById("currentLocation").innerHTML = currentLocation;
-  //     new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
-  //   })
-  //   .catch(error => console.log(error))
-  // }
 
   function waitForGoogle() {
     if (typeof google==='undefined') {
@@ -41,3 +30,6 @@ import {geocodeLocation} from './geocode.js'
     }
   }
   waitForGoogle();
+
+  let element = document.getElementsByClassName('text-city__button')
+  element[0].addEventListener("click", getLocation);
