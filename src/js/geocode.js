9d3915d618aed2f607bac66b5c205229ea790bbb
Hans Christian Reinl <info@drublic.de>
2018-12-16 12:23:18 +0100

    Clean code, fix issue with missing `map` param

--- src/js/geocode.js
+++ src/js/geocode.js
@@ -1,13 +1,22 @@
-export function geocodeLocation(lat, lng) {
-  const url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=AIzaSyDZhHS4OYBe0AGr1cByePHG-pz8ORJ1KtU"
+export function geocodeLocation(map, lat, lng) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyDZhHS4OYBe0AGr1cByePHG-pz8ORJ1KtU`;
+
+  const element = document.querySelector("[data-current-location]");
+
   fetch(url)
-    .then(res => {
-      return res.json()
+    .then(response => response.json())
+    .then((data) => {
+      let currentLocation = data.results[0].address_components[2].long_name;
+
+      element.innerText = currentLocation;
+
+      new google.maps.Marker({
+        position: {
+          lat,
+          lng
+        },
+        map,
+      });
     })
-    .then(data => {
-      let currentLocation = (data.results[0].address_components[2].long_name);
-      document.getElementById("currentLocation").innerHTML = currentLocation;
-      // new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
-    })
-    .catch(error => console.log(error))
+    .catch(error => console.error(error))
 }
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,8 +5,10 @@ import {
 // Initialisierung der Karte mit Zentrierung auf das Waldstadion
 let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+const initMap = () => {
+  const element = document.getElementById('map');
+
+  map = new google.maps.Map(element, {
     center: {
       lat: 50.068611,
       lng: 8.645278
@@ -16,39 +18,38 @@ function initMap() {
 };
 
 // Standort ermitteln, Karte darauf zentrieren, mit Koordinaten Geocode aufrufen und ausgeben im Textfeld
-function getLocation() {
-  const url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDZhHS4OYBe0AGr1cByePHG-pz8ORJ1KtU"
+const getLocation = () => {
+  const url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDZhHS4OYBe0AGr1cByePHG-pz8ORJ1KtU";
+
   fetch(url, {
-      method: "POST"
-    })
-    .then(res => {
-      return res.json()
-    })
-    .then(data => {
+    method: "POST"
+  })
+    .then(res => res.json())
+    .then(({ location }) => {
+      const { lat, lng } = location;
+
       map.setCenter({
-        lat: data.location.lat,
-        lng: data.location.lng
+        lat,
+        lng
       });
-      geocodeLocation(data.location.lat, data.location.lng);
-      new google.maps.Marker({
-        position: {
-          lat: data.location.lat,
-          lng: data.location.lng
-        },
-        map: map
-      });
+
+      geocodeLocation(map, lat, lng);
     })
     .catch(error => console.log(error))
-}
+};
 
-function waitForGoogle() {
-  if (typeof google === 'undefined') {
-    setTimeout(() => waitForGoogle(), 1000);
-  } else {
-    initMap();
+const waitForGoogle = () => {
+  if (typeof google !== 'undefined') {
+    return initMap();
   }
-}
-waitForGoogle();
 
-const element = document.getElementsByClassName('text-city__button')
-element[0].addEventListener("click", getLocation);
+  setTimeout(() => waitForGoogle(), 1000);
+};
+
+(() => {
+  waitForGoogle();
+
+  const element = document.getElementsByClassName('text-city__button')
+
+  element[0].addEventListener("click", getLocation);
+})();
